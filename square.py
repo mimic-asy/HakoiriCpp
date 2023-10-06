0af4056a6f3f550dc40796c0c3c44cff8f8bf5fb
Mizuki Asai <mimic0924444@gmail.com>
2023-10-06 14:06:07 +0900

    初期盤面を通常時に変更、盤面が反転するバグの改良

--- square.py
+++ square.py
@@ -4,7 +4,13 @@ import numpy as np
 import csv
 import matplotlib.patches as patches
 
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Hiragino Maru Gothic Pro', 'Yu Gothic', 'Meirio', 'Takao', 'IPAexGothic', 'IPAPGothic', 'VL PGothic', 'Noto Sans CJK JP']
+
+
 lis = []
+
 
 with open('/home/poyoyonland/projects/Hakoiricpp/build/example.csv') as puzzle:
     reader = csv.reader(puzzle)
@@ -16,25 +22,61 @@ with open('/home/poyoyonland/projects/Hakoiricpp/build/example.csv') as puzzle:
 
 def write_board(Matrix):
     fig, ax = plt.subplots()
+
     bigrect = patches.Rectangle((0, 0), width=4, height=5, linewidth=20, edgecolor="#575757", facecolor="#FFFFFF")
     ax.add_patch(bigrect)
     bigrect2 = patches.Rectangle((0, 0), width=4, height=5, linewidth=2, edgecolor="#575757", facecolor="#FFFFFF")
     ax.add_patch(bigrect2)
-    shortrect = patches.Rectangle((1, 0), width=2, height=0, linewidth=22, edgecolor="#FFFFFF", facecolor="#FFFFFF")
+    shortrect = patches.Rectangle((1, 5), width=2, height=0, linewidth=22, edgecolor="#FFFFFF", facecolor="#FFFFFF")
     ax.add_patch(shortrect)
 
+    ax.invert_yaxis()
     for i in range(10):
         ys, xs = np.where(Matrix == i)
         w = xs.max() - xs.min() + 1
         h = ys.max() - ys.min() + 1
         col = color(i)
         rect = patches.Rectangle((xs.min(), ys.min()), w, h, linewidth=4, ec="#575757", fc=col)
+
+
         ax.add_patch(rect)
+
+        center_x = xs.min() + w / 2
+        center_y = ys.min() + h / 2
+
+
+        text = name(i)
+        ax.annotate(text, xy=(center_x, center_y), ha='center', va='center', fontsize=12)
+
 
 
     ax.set_aspect('equal')
     ax.set_axis_off()
     plt.axis('scaled')
+
+
+def name(i):
+    if(i == 1):
+        return "父親"
+    elif(i == 2):
+        return "母親"
+    elif(i == 3):
+        return "祖父"
+    elif(i == 4):
+        return "祖母"
+    elif(i == 5):
+        return "兄弟"
+    elif(i == 6):
+        return "華道"
+    elif(i == 7):
+        return "茶道"
+    elif(i == 8):
+        return "書道"
+    elif(i == 9):
+        return "武道"
+    elif(i == 0):
+        return "娘"
+
 
 
 def color(i):
