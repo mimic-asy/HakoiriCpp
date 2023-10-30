bf6a5cfe2ebb50e39d9cab0525f52d80ecc98b25
Mizuki Asai <mimic0924444@gmail.com>
2023-10-31 01:52:44 +0900

    ダイクストラ法での最短経路探索を実装

--- shortest_clear_logic.cpp
+++ shortest_clear_logic.cpp
@@ -7,7 +7,7 @@
 #include "puzzle/puzzle_logic.hpp"
 #include "puzzle/graph_logic.hpp"
 
-inline void node::set_node(Matrix54i puz, int cos, node nd)
+inline void node::set_node(const Matrix54i puz, const int cos, const node &nd)
 {
     puzzle = puz;
     cost = cos;
@@ -20,7 +20,7 @@ bool isvalue_already(const std::unordered_map<std::string, std::string, KeyHashe
     return it != puzzle_index.end();
 }
 
-void make_newnode(const Matrix54i state, int i, node new_node, node now)
+void make_newnode(const Matrix54i state, const int i, node &new_node, const node &now)
 {
     new_node.puzzle = state;
     new_node.cost = i;
@@ -49,6 +49,7 @@ void dikstrqueue(const int i,
         {
             node new_node;
             make_newnode(state, i, new_node, now);
+            std::cout << new_node.cost << std::endl;
             edges[state_simple] = new_node;
             puzzle_index[hash_puzzle] = hash_puzzle;
             puzzle_list.push(state);
@@ -56,10 +57,9 @@ void dikstrqueue(const int i,
             if (clear(state))
             {
                 std::vector<node> route;
-                while (new_node.cost>0)
+                while (new_node.cost > 0)
                 {
                     route.push_back(new_node);
-                    std::cout << new_node.cost << std::endl;
                     new_node = dikstr(new_node);
                 }
                 clear_route.push_back(route);
@@ -98,15 +98,18 @@ std::vector<std::vector<node>> breadth_first_search_dikstr(const Matrix54i &puzz
     int i = 0;
     while (!puzzle_list.empty())
     {
-        i++;
+
         const Matrix54i now_puzzle = puzzle_list.front();
         puzzle_list.pop();
         std::vector<Matrix54i> movable = moved_board_list(now_puzzle);
+        i++;
         dikstrqueue(i, now_puzzle, movable,
                     puzzle_index, edges, clear_route, puzzle_list);
     }
+
+    std::vector<node> s = shortestroute_find_dikstr(clear_route);
     std::cout << "総手数は" << puzzle_index.size() << "手です" << std::endl;
-    std::cout << "クリアルートは" << clear_route.size() << "手です" << std::endl;
+    std::cout << "クリアルートは" << s.size() << "手です" << std::endl;
 
     return clear_route;
 }
@@ -121,39 +124,17 @@ node dikstr(node now_node)
     {
         if (min == -1)
         {
-            min = n.cost;
             min_node = n;
+            min = min_node.cost;
         }
         if (min > n.cost)
         {
-            min = n.cost;
             min_node = n;
-        }
-        else if (min == n.cost)
-        {
-            min_node = dikstrloop(min_node, n);
+            min = min_node.cost;
         }
     }
 
     return min_node;
-}
-
-node dikstrloop(node min, node n)
-{
-    node min_next = dikstr(min);
-    node n_next = dikstr(n);
-    if (min_next.cost > n_next.cost)
-    {
-        return n_next;
-    }
-    else if (min_next.cost == n_next.cost)
-    {
-        return dikstrloop(min_next, n_next);
-    }
-    else
-    {
-        return min_next;
-    }
 }
 
 std::vector<node> shortestroute_find_dikstr(const std::vector<std::vector<node>> &c_route)
